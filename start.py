03f723d09ea98f5cab9b04d218ea441d99ed5aa7
Suryabhat48 <suryabhat2032@gmail.com>
2026-05-25 11:24:28 +0530

    Learnt somethings about variables and data types

--- start.py
+++
@@ -1,8 +0,0 @@
-print("Hello, World!")
-# We will start from the first thing and this is the chappter 1  Variables and Data types 
-print("Hey my name is Surya","and I am 19 years old  and I am a student of computer science engineering and AIML")
-# Variables are used to store data in a program. In Python, you can create a variable by simply assigning a value to it. For example:``
-name = "Surya"
-age = 20
-print(name)
-print(age)
---
+++ var_dat.py
@@ -0,0 +1,29 @@
+print("Hello, World!")
+# We will start from the first thing and this is the chappter 1  Variables and Data types 
+print("Hey my name is Surya","and I am 19 years old  and I am a student of computer science engineering and AIML")
+# Variables are used to store data in a program. In Python, you can create a variable by simply assigning a value to it. For example:``
+name = "Surya"
+age = 20
+print(name)
+print(age)
+# So basically in python we have five data types Sring,,Integer,Float,Boolean and None 
+# a=None
+# print(a)
+# basically by none we mean we cant assign any value for that variable specifically use the capital T and F for boolean types
+# Program to find sum of two numbers 
+a=10
+b=20
+sum=a+b
+print("The sum of a and b is :",sum)
+# There are two types of comments which we write single line comment and multi line comment for single line comment we use # and for multi line comment we use """ on both sides """
+# Input taking functionn in user is used to take input from the user input() function 
+name=input("Enter your name:")
+
+# By default input function takes input as a string if we want to take the input in other forms we basically have to typecast things
+age=int(input("Enter your age:"))
+
+# We can also take input in float form
+height=float(input("Enter your height in cm:"))
+print("Hello this is ",name)
+print("Your age is ",age,"years")
+print("Your height is ",height,"cm")    
